f57fba0f7b2a17b5ccb240d0c8ad2e102d514e42
Adarsh <iadarshh@gmail.com>
2023-08-15 18:42:20 +0530

    emailtrap testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,14 +26,25 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['MAILTRAP_USERNAME'],
+  #   :password => ENV['MAILTRAP_PASSWORD'],
+  #   :address => 'sandbox.smtp.mailtrap.io',
+  #   :host => 'sandbox.smtp.mailtrap.io',
+  #   :port => '2525',
+  #   :authentication => :cram_md5
+  # }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['MAILTRAP_USERNAME'],
-    :password => ENV['MAILTRAP_PASSWORD'],
-    :address => 'sandbox.smtp.mailtrap.io',
-    :host => 'sandbox.smtp.mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
+    address:              'smtp.yourserver.com',
+    port:                 25,
+    domain:               'https://rails-photo-app-b375e341725e.herokuapp.com/',
+    user_name:            ENV['MAILTRAP_USERNAME'],
+    password:             ENV['MAILTRAP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
