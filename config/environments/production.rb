53fa700240135edd4a402c5965cb67d25d3b92c6
Adarsh <21220825+adar5h@users.noreply.github.com>
2023-08-19 12:47:28 +0000

    added GMAIL SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,21 +70,14 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.action_mailer.delivery_method = :smtp   host = "rails-photo-app-b375e341725e.herokuapp.com"   
 
-config.action_mailer.default_url_options = { host: host, protocol: 'https' }   ActionMailer::Base.smtp_settings = {     
-
-:address => 'smtp.gmail.com',
-
-:port => '587',
-
-:authentication => :plain,
-
-:user_name => ENV['GMAIL_USERNAME'],     
-
-:password => ENV['GMAIL_PASSWORD'],     
-
-:domain => 'gmail.com',   
-
-:enable_starttls_auto => true }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }   ActionMailer::Base.smtp_settings = {     
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['GMAIL_USERNAME'],     
+    :password => ENV['GMAIL_PASSWORD'],     
+    :domain => 'gmail.com',   
+    :enable_starttls_auto => true }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
