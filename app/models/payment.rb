6a957bfb52fd1a027c360a189ab51c9f5e35736f
Adarsh <iadarshh@gmail.com>
2023-08-27 00:17:45 +0530

    changing Stripe Charge to Stripe Payment Intent

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -15,7 +15,7 @@ class Payment < ApplicationRecord
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
 
-    Stripe::Charge.create customer: customer.id,
+    Stripe::PaymentIntent.create customer: customer.id,
                           amount: 1000, #cents
                           description: 'Premium',
                           currency: 'usd'
